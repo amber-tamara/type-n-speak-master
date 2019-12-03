a79ae26fec7cf174d366d5f22251555d76cffb3e
Amber Tamara <ambertjansen@gmail.com>
2019-12-03 16:30:32 +0000

    (almost finished, need to add border and fix pitch)

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const rate = document.querySelector("#rate");
 const rateValue = document.querySelector("#rate-value");
 const pitch = document.querySelector("#pitch");
 const pitchValue = document.querySelector("#pitch-value");
-
+const body = document.querySelector("body");
 //init voices array
 let voices = [];
 
@@ -21,7 +21,7 @@ const getVoices = () => {
     // Create option element
     const option = document.createElement("option");
     //Fill option with voice and language
-    option.textContent = voice.name + "(" + voice.lang + ")";
+    option.textContent = voice.name + " (" + voice.lang + ")";
 
     // Set needed option attributes
     option.setAttribute("data-lang", voice.lang);
@@ -34,3 +34,68 @@ getVoices();
 if (synth.onvoiceschanged !== undefined) {
   synth.onvoiceschanged = getVoices;
 }
+
+//Speak
+const speak = () => {
+  // Check if speaking
+  if (synth.speaking) {
+    console.error("Already speaking..");
+    return; //this functions gone far enough
+  }
+  if (textInput.value !== "") {
+    // Add background animation
+    body.style.background = "#141414 url(../img/wave.gif)";
+    body.style.backgroundRepeat = "repeat-x"; //Repeats only horizoltly
+    body.style.backgroundSize = "100% 100%"; //covers whole background
+    // Get speak text
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+    // Speak end
+    speakText.onend = e => {
+      console.log("Done speaking...");
+      body.style.background = "#141414";
+    };
+    // Speak error
+    speakText.onerror = e => {
+      console.error("Something went wrong");
+    };
+
+    // Selected voice
+    // store the name of the voice option the user has chosen
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
+      "data-name"
+    );
+
+    // Loop through voices
+    voices.forEach(voice => {
+      if (voice.name === selectedVoice) {
+        speakText.voice = voice;
+      }
+    });
+
+    // Set pitch and rate
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+    //Speak
+    synth.speak(speakText);
+  }
+};
+
+// EVENT LISTENERS
+
+// text form submit
+textForm.addEventListener("submit", e => {
+  //Is listening for a submit
+  console.log(e);
+  e.preventDefault(); //E has properties
+  speak();
+  textInput.blur(); //Blur de-selects the options, otherwise by default is stays selected.
+});
+
+// Rate value change
+rate.addEventListener("change", e => (rateValue.textContent = rate.value));
+
+// Pitch value change
+pitch.addEventListener("change", e => (pitchValue.textContent = rate.value));
+
+// Voice select change
+voiceSelect.addEventListener("change", e => speak()); //Is listening for a change
